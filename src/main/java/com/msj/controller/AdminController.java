656e01a19ff63a7b134c04062d21617e74e35a71
shengjie <2683284969@qq.com>
2020-10-14 17:09:27 +0800

    email_system

--- src/main/java/com/msj/controller/AdminController.java
+++ src/main/java/com/msj/controller/AdminController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -43,5 +44,39 @@ public class AdminController {
         msg = "登录失败";
         request.setAttribute("msg", msg);
         return "admin/login";
+    }
+
+    @RequestMapping("/adminList")
+    public String adminList(Model model) {
+        model.addAttribute("adminList", adminService.getAll());
+        return "admin/admin_list";
+    }
+
+    /*添加管理员*/
+    @RequestMapping("/adminAdd")
+    public String adminAdd() {
+        return "admin/admin_add";
+    }
+
+    @RequestMapping("/adminSave")
+    public String adminSave(Admin admin) {
+        admin.setPassword(MD5Utils.md5Password(admin.getPassword()));
+        adminService.insert(admin);
+        return "redirect:/admin/adminList";
+
+    }
+
+    /*重置密码*/
+    @RequestMapping("/adminRe")
+    public String adminRe(@RequestParam("id") int id) {
+        return "redirect:/admin/adminList";
+    }
+
+    /*删除管理员*/
+    @RequestMapping("/adminDelete")
+    public String adminDelete(@RequestParam("id") int id) {
+        adminService.delete(id);
+        return "redirect:/admin/adminList";
+
     }
 }
--- src/main/webapp/admin/header.jsp
+++ src/main/webapp/admin/header.jsp
@@ -20,7 +20,7 @@
                 <li <c:if test="${flag==2}">class="active"</c:if>><a href="/goods/goodList">商品管理</a></li>
                 <li <c:if test="${flag==3}">class="active"</c:if>><a href="orderList">订单管理</a></li>
                 <li <c:if test="${flag==4}">class="active"</c:if>><a href="userList">用户管理</a></li>
-                <li <c:if test="${flag==5}">class="active"</c:if>><a href="adminList">管理员</a></li>
+                <li <c:if test="${flag==5}">class="active"</c:if>><a href="/admin/adminList">管理员</a></li>
                 <li><a href="logout">退出</a></li>
             </ul>
         </div>
